a6cbe25ede214577909c0066bcd7065df6494221
shixiankang <shixiankang@vv.cn>
2024-06-25 14:55:24 +0800

    test: 测试文件

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from "./App.vue";
 import echarts from "echarts";
 
 // Vue.config.productionTip = false;
-Vue.prototype.$echarts = echarts;
+// Vue.prototype.$echarts = echarts;
 
 new Vue({
   render: (h) => h(App),
